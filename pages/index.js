fa17dc38935418d31f0ff0e02d61af87912144d7
Jesús Enrique Rascón <blograso@gmail.com>
2021-01-08 23:01:27 +0100

    abdu thancc

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       <Layout>
         <div className="max-w-4xl mx-auto text-white m-20">
           <h1 className="text-8xl font-bold text-center">
-            Welcome to my lovely place!
+            Welcome to my lovely place!💖
           </h1>
           <TopTracks className="grid grid-cols-2" />
         </div>
