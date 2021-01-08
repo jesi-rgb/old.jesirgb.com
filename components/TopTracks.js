c79dfd152ed599aa118e5f3023347751c2313a63
Jesús Enrique Rascón <blograso@gmail.com>
2021-01-08 20:03:05 +0100

    changed margin

--- components/TopTracks.js
+++ components/TopTracks.js
@@ -12,8 +12,8 @@ export default function TopTracks() {
 
   return (
     <>
-      <div className="w-96 mx-auto border-2 p-3 rounded-lg">
-        <p className="text-2xl text-center mb-3">
+      <div className="w-96 mx-auto border-2 p-3 rounded-lg mb-10 mt-16">
+        <p className="text-2xl text-center my-3">
           Here's a list of my last played songs... click on them to listen!
         </p>
         {data.tracks.map((track, index) => (
