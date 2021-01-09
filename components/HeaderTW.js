540637c457f1a44fcd77ffc890e907936a8ce160
Jesús Enrique Rascón <blograso@gmail.com>
2021-01-09 01:50:26 +0100

    tailwind clean up of footer!

--- components/HeaderTW.js
+++ components/HeaderTW.js
@@ -1,43 +1,42 @@
 export const HeaderTW = () => {
   return (
     <>
-      <div className="relative bg-accent-1 py-6 mb-6">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6">
-          <div className="flex justify-between items-center py-6 md:justify-start md:space-x-10">
-            <div className="flex lg:w-0 lg:flex-1">
+      <div className="relative bg-accent-1 py-6 mb-20">
+        <div className="max-w-7xl mx-auto">
+          <div className="flex items-center py-6">
+            <div className="flex flex-1">
               <a href="/">
                 <img
-                  className="transform scale-250 sm:h-10"
+                  className="transform scale-250 h-10"
                   src="/jesi.svg"
                   alt=""
                 />
               </a>
             </div>
-            <div className="-mr-2 -my-2"></div>
-            <nav className="hidden md:flex space-x-10">
+            <nav className="flex space-x-10">
               <a
                 href="/"
-                className="text-base font-medium text-white text-4xl font-bold hover:text-accent-1-hover transition-colors"
+                className="text-white text-4xl font-bold hover:text-accent-1-hover transition-colors"
               >
                 Thinking thoughts
               </a>
             </nav>
-            <nav className="hidden md:flex items-right space-x-10 justify-end md:flex-1 lg:w-0">
+            <nav className="flex items-right space-x-10 justify-end flex-1">
               <a
                 href="/blog"
-                className="whitespace-nowrap text-base text-2xl font-medium text-white hover:text-accent-1-hover transition-colors"
+                className="whitespace-nowrap text-2xl font-medium text-white hover:text-accent-1-hover transition-colors"
               >
                 Blog
               </a>
               <a
                 href="https://jesus-enrique-cv.jesi-rgb.vercel.app"
-                className="whitespace-nowrap text-base text-2xl font-medium text-white hover:text-accent-1-hover transition-colors"
+                className="whitespace-nowrap text-2xl font-medium text-white hover:text-accent-1-hover transition-colors"
               >
                 Curriculum
               </a>
               <a
                 href="/about"
-                className="whitespace-nowrap text-base text-2xl font-medium text-white hover:text-accent-1-hover transition-colors"
+                className="whitespace-nowrap text-2xl font-medium text-white hover:text-accent-1-hover transition-colors"
               >
                 About
               </a>
