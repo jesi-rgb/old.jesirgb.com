a839bb414043cac8fe698d2d4c1bad6e16555399
Jesús Enrique Rascón <blograso@gmail.com>
2021-01-10 16:07:44 +0100

    made footer responsive

--- components/Footer.js
+++ components/Footer.js
@@ -19,19 +19,19 @@ config.autoAddCss = false; /* eslint-disable import/first */
 export default function Footer() {
   return (
     <footer className="items-center border-t-2 border-gray-200 text-gray-300 mt-32">
-      <div className="py-10 flex flex-row items-center">
+      <div className="py-8 space-y-5 md:space-y-0 md:py-10 flex flex-col md:flex-row items-center">
         <Link href="/">
-          <a className="w-2/6 text-5xl font-bold tracking-tighter leading-tight text-left  hover:text-accent-1 transition-colors">
+          <a className="text-3xl mb-2 md:my-0 md:w-2/6 font-bold tracking-tighter leading-tight text-left hover:text-accent-1 transition-colors">
             <h3>Jesús Rascón</h3>
           </a>
         </Link>
 
-        <div className="w-2/6 items-center text-center">
+        <div className="flex flex-row text-center items-center my-2 md:my-0 md:w-2/6">
           <Link href="https://www.instagram.com/jrascon_">
             <a>
               <FontAwesomeIcon
                 icon={faInstagram}
-                className="transform scale-250 mr-7 hover:text-accent-1 transition-colors"
+                className="transform scale-150 md:scale-250 mr-7 hover:text-accent-1 transition-colors"
               />
             </a>
           </Link>
@@ -40,7 +40,7 @@ export default function Footer() {
             <a>
               <FontAwesomeIcon
                 icon={faTwitter}
-                className="transform scale-250 mx-7 hover:text-accent-1 transition-colors"
+                className="transform scale-150 md:scale-250 mx-7 hover:text-accent-1 transition-colors"
               />
             </a>
           </Link>
@@ -49,7 +49,7 @@ export default function Footer() {
             <a>
               <FontAwesomeIcon
                 icon={faGithub}
-                className="transform scale-250 mx-7 hover:text-accent-1 transition-colors"
+                className="transform scale-150 md:scale-250 mx-7 hover:text-accent-1 transition-colors"
               />
             </a>
           </Link>
@@ -58,7 +58,7 @@ export default function Footer() {
             <a>
               <FontAwesomeIcon
                 icon={faLinkedin}
-                className="transform scale-250 mx-7 hover:text-accent-1 transition-colors"
+                className="transform scale-150 md:scale-250 mx-7 hover:text-accent-1 transition-colors"
               />
             </a>
           </Link>
@@ -66,12 +66,12 @@ export default function Footer() {
             <a>
               <FontAwesomeIcon
                 icon={faTelegram}
-                className="transform scale-250 ml-7 hover:text-accent-1 transition-colors"
+                className="transform scale-150 md:scale-250 ml-7 hover:text-accent-1 transition-colors"
               />
             </a>
           </Link>
         </div>
-        <h4 className="text-5xl w-2/6 font-bold tracking-tighter text-right">
+        <h4 className="text-xl md:text-3xl my-3 md:w-2/6 md:my-0 font-bold tracking-tighter text-right">
           2021
         </h4>
       </div>
--- components/HeaderTW.js
+++ components/HeaderTW.js
@@ -1,47 +1,44 @@
+import Image from "next/image";
+
 export const HeaderTW = () => {
   return (
     <>
-      <div className="relative bg-accent-1 py-6 mb-6">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6">
-          <div className="flex justify-between items-center py-6 md:justify-start md:space-x-10">
-            <div className="flex lg:w-0 lg:flex-1">
-              <a href="/">
-                <img
-                  className="transform scale-250 sm:h-10"
-                  src="/jesi.svg"
-                  alt=""
-                />
-              </a>
-            </div>
-            <div className="-mr-2 -my-2"></div>
-            <nav className="hidden md:flex space-x-10">
-              <a
-                href="/"
-                className="text-base font-medium text-white text-4xl font-bold hover:text-accent-1-hover transition-colors"
-              >
-                Thinking thoughts
-              </a>
-            </nav>
-            <nav className="hidden md:flex items-right space-x-10 justify-end md:flex-1 lg:w-0">
-              <a
-                href="/blog"
-                className="whitespace-nowrap text-base text-2xl font-medium text-white hover:text-accent-1-hover transition-colors"
-              >
-                Blog
-              </a>
-              <a
-                href="https://jesus-enrique-cv.jesi-rgb.vercel.app"
-                className="whitespace-nowrap text-base text-2xl font-medium text-white hover:text-accent-1-hover transition-colors"
-              >
-                Curriculum
-              </a>
-              <a
-                href="/about"
-                className="whitespace-nowrap text-base text-2xl font-medium text-white hover:text-accent-1-hover transition-colors"
-              >
-                About
-              </a>
-            </nav>
+      {/* header itself */}
+      <div className="relative bg-accent-1 mb-10 md:py-6 md:mb-20">
+        {/* div for all three elements */}
+        <div className="flex flex-row mx-auto items-center md:py-6">
+          {/* logo */}
+          <div className="m-2 justify-center align-middle w-1/2 md:w-1/4 md:mx-10 md:transform md:scale-250">
+            <a href="/">
+              <img src="/jesi.svg" alt="" width={50} height={50} />
+            </a>
+          </div>
+
+          {/* title */}
+          <div className="hidden text-white text-4xl font-bold hover:text-accent-1-hover transition-colors md:visible md:flex md:items-center md:w-2/4">
+            <a href="/">Thinking thoughts</a>
+          </div>
+
+          {/* navigators */}
+          <div className="w-1/2 text-md space-x-3 mx-4 font-medium md:justify-end md:w-1/2 md:text-2xl">
+            <a
+              href="/blog"
+              className="text-white hover:text-accent-1-hover transition-colors"
+            >
+              Blog
+            </a>
+            <a
+              href="https://jesus-enrique-cv.jesi-rgb.vercel.app"
+              className="text-white hover:text-accent-1-hover transition-colors"
+            >
+              Curriculum
+            </a>
+            <a
+              href="/about"
+              className="text-white hover:text-accent-1-hover transition-colors"
+            >
+              About
+            </a>
           </div>
         </div>
       </div>
