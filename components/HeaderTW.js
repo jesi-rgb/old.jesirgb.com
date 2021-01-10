f4a8a4ef242e8a8bcfc3f2fdbde814d22a35eb01
Jesús Enrique Rascón <blograso@gmail.com>
2021-01-10 10:45:09 +0100

    header partially responsive

--- components/HeaderTW.js
+++ components/HeaderTW.js
@@ -1,46 +1,44 @@
+import Image from "next/image";
+
 export const HeaderTW = () => {
   return (
     <>
-      <div className="relative bg-accent-1 py-6 mb-20">
-        <div className="max-w-7xl mx-auto">
-          <div className="flex items-center py-6">
-            <div className="flex flex-1">
-              <a href="/">
-                <img
-                  className="transform scale-250 h-10"
-                  src="/jesi.svg"
-                  alt=""
-                />
-              </a>
-            </div>
-            <nav className="flex space-x-10">
-              <a
-                href="/"
-                className="text-white text-4xl font-bold hover:text-accent-1-hover transition-colors"
-              >
-                Thinking thoughts
-              </a>
-            </nav>
-            <nav className="flex items-right space-x-10 justify-end flex-1">
-              <a
-                href="/blog"
-                className="whitespace-nowrap text-2xl font-medium text-white hover:text-accent-1-hover transition-colors"
-              >
-                Blog
-              </a>
-              <a
-                href="https://jesus-enrique-cv.jesi-rgb.vercel.app"
-                className="whitespace-nowrap text-2xl font-medium text-white hover:text-accent-1-hover transition-colors"
-              >
-                Curriculum
-              </a>
-              <a
-                href="/about"
-                className="whitespace-nowrap text-2xl font-medium text-white hover:text-accent-1-hover transition-colors"
-              >
-                About
-              </a>
-            </nav>
+      {/* header itself */}
+      <div className="relative bg-accent-1 mb-10 lg:py-6 lg:mb-20">
+        {/* div for all three elements */}
+        <div className="flex flex-row mx-auto items-center lg:py-6">
+          {/* logo */}
+          <div className="m-2 justify-center align-middle w-1/2 lg:w-1/4 lg:mx-10 lg:transform lg:scale-250">
+            <a href="/">
+              <img src="/jesi.svg" alt="" width={50} height={50} />
+            </a>
+          </div>
+
+          {/* title */}
+          <div className="hidden text-white text-4xl font-bold hover:text-accent-1-hover transition-colors lg:visible lg:flex lg:items-center lg:w-2/4">
+            <a href="/">Thinking thoughts</a>
+          </div>
+
+          {/* navigators */}
+          <div className="w-1/2 text-md space-x-3 mx-4 font-medium lg:justify-end lg:w-1/2 lg:text-2xl">
+            <a
+              href="/blog"
+              className="text-white hover:text-accent-1-hover transition-colors"
+            >
+              Blog
+            </a>
+            <a
+              href="https://jesus-enrique-cv.jesi-rgb.vercel.app"
+              className="text-white hover:text-accent-1-hover transition-colors"
+            >
+              Curriculum
+            </a>
+            <a
+              href="/about"
+              className="text-white hover:text-accent-1-hover transition-colors"
+            >
+              About
+            </a>
           </div>
         </div>
       </div>
