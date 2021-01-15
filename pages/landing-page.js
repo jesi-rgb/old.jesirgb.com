5f5d42cd452beba9817371879f11d8fd05a78d04
Jesús Enrique Rascón <blograso@gmail.com>
2021-01-15 16:18:37 +0100

    even better design!

--- pages/landing-page.js
+++ pages/landing-page.js
@@ -4,120 +4,47 @@ export default function Landing() {
   return (
     <>
       <svg
-        width="100%"
-        height="100%"
-        viewBox="70 0 650 650"
+        width="3000"
+        height="2500"
+        viewBox="60 0 650 650"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <rect x="78" width="627" height="1219" fill="#242424" />
-        <circle
-          cx="371.5"
-          cy="314.5"
-          r="77.5"
-          fill="#C4A8FF"
-          fill-opacity="0.4"
-          style={{ "mix-blend-mode": "lighten" }}
-        />
-        <circle
-          cx="465.5"
-          cy="263.5"
-          r="33.5"
-          fill="#EEFFA8"
-          fill-opacity="0.4"
-          style={{ "mix-blend-mode": "lighten" }}
-        />
-        <circle
-          cx="497.5"
-          cy="350.5"
-          r="33.5"
-          fill="#5055DD"
-          fill-opacity="0.4"
-          style={{ "mix-blend-mode": "lighten" }}
-        />
-        <circle
-          cx="546"
-          cy="455"
-          r="4"
-          fill="#5055DD"
-          fill-opacity="0.4"
-          style={{ "mix-blend-mode": "lighten" }}
-        />
-        <circle
-          cx="453"
-          cy="441"
-          r="4"
-          fill="#5055DD"
-          fill-opacity="0.4"
-          style={{ "mix-blend-mode": "lighten" }}
-        />
-        <circle
-          cx="554"
-          cy="412"
-          r="4"
-          fill="#5055DD"
-          fill-opacity="0.4"
-          style={{ "mix-blend-mode": "lighten" }}
-        />
-        <circle
-          cx="461"
-          cy="398"
-          r="4"
-          fill="#5055DD"
-          fill-opacity="0.4"
-          style={{ "mix-blend-mode": "lighten" }}
-        />
-        <circle
-          cx="514"
-          cy="459"
-          r="4"
-          fill="#5055DD"
-          fill-opacity="0.4"
-          style={{ "mix-blend-mode": "lighten" }}
-        />
-        <circle
-          cx="522"
-          cy="416"
-          r="4"
-          fill="#5055DD"
-          fill-opacity="0.4"
-          style={{ "mix-blend-mode": "lighten" }}
-        />
-        <circle
-          cx="495"
-          cy="420"
-          r="15"
-          fill="#5055DD"
-          fill-opacity="0.4"
-          style={{ "mix-blend-mode": "lighten" }}
-        />
-        <circle
-          cx="432.5"
-          cy="399.5"
-          r="7.5"
-          fill="#5055DD"
-          fill-opacity="0.4"
-          style={{ "mix-blend-mode": "lighten" }}
-        />
-        <circle
-          cx="452"
-          cy="372"
-          r="12"
-          fill="#5055DD"
-          fill-opacity="0.4"
-          style={{ "mix-blend-mode": "lighten" }}
-        />
-        <circle
-          cx="249"
-          cy="180"
-          r="171"
-          fill="#1ABC9C"
-          fill-opacity="0.4"
-          style={{ "mix-blend-mode": "lighten" }}
-        />
+        <svg
+          width="540"
+          height="540"
+          viewBox="0 -5 540 529"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle
+            style={{ "mix-blend-mode": "lighten" }}
+            cx="171"
+            cy="171"
+            r="171"
+            fill="#1ABC9C"
+            id="circle-top"
+          />
+          <circle
+            style={{ "mix-blend-mode": "lighten" }}
+            cx="369"
+            cy="242"
+            r="171"
+            fill="#9C88DD"
+            id="circle-right"
+          />
+          <circle
+            style={{ "mix-blend-mode": "lighten" }}
+            cx="206"
+            cy="358"
+            r="171"
+            fill="#0000FF"
+            id="circle-bot"
+          />
+        </svg>
       </svg>
 
-      <div className="absolute text-white text-3xl font-light m-40 top-1 z-10">
+      <div className="absolute w-full text-white text-3xl font-light m-40 top-44">
         I am{" "}
         <h1 className="text-9xl 2xl:text-10xl m-10 font-bold font-blogpost">
           Jesús Rascón
@@ -125,6 +52,37 @@ export default function Landing() {
         <div className="my-32 w-1/3 font-light">
           Data driven journalist, frontend developer, musician and creative.
         </div>
+        <style jsx>{`
+          #circle-top {
+            animation-name: fill 5s infinite linear;
+            @keyframes fill {
+              12.5% {
+                fill: #ff0000;
+              }
+              25% {
+                fill: #ffa500;
+              }
+              37.5% {
+                fill: #ffff00;
+              }
+              50% {
+                fill: #7fff00;
+              }
+              62.5% {
+                fill: #00ffff;
+              }
+              75% {
+                fill: #0000ff;
+              }
+              87.5% {
+                fill: #9932cc;
+              }
+              100% {
+                fill: #ff1493;
+              }
+            }
+          }
+        `}</style>
       </div>
     </>
   );
