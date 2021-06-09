7de0e4f77660ce9e8c7280f05c32787ce763adbf
Jesús Enrique Rascón <blograso@gmail.com>
2021-06-09 13:46:14 +0200

    editing of some properties

--- components/BlogItem.js
+++ components/BlogItem.js
@@ -1,7 +1,7 @@
 export default function BlogItem({ slug, title, description, date }) {
   return (
     <a href={"/blog/" + slug}>
-      <div className="link border-t border-gray-400 my-16 font-display text-white">
+      <div className="link border-t border-gray-400 mb-16 font-display text-white">
         <div className="flex flex-row max-w-sm xl:max-w-2xl">
           <div className="font-bold mt-3 text-xl text-left align-bottom w-1/2">
             {title}
