0feb3eb1e983214d1677e76aec5e0838fd4a44ff
Jesús Enrique Rascón <blograso@gmail.com>
2021-09-06 11:55:58 +0200

    commmit prueba

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { HeaderLanding } from "@/components/HeaderLanding";
 import TopTracks from "@/components/TopTracks";
 import Head from "next/head";
 
-// no se
+// no s
 export default function Landing() {
   return (
     <>
