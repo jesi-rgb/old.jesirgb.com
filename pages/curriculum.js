c634af376b4a116de7f27f33ec4c6c9596668863
Jesús Enrique Rascón <blograso@gmail.com>
2021-10-31 20:37:19 +0100

    compled and redesigned experience and education

--- pages/curriculum.js
+++ pages/curriculum.js
@@ -2,16 +2,17 @@ import Layout from "@/components/Layout";
 import Head from "next/head";
 
 import {
-  faInstagram,
-  faTwitter,
-  faGithub,
-  faLinkedin,
-  faTelegram,
-} from "@fortawesome/free-brands-svg-icons";
-
-import { faCalendar, faLocationArrow } from "@fortawesome/free-solid-svg-icons";
+  faCalendar,
+  faLocationArrow,
+  faChevronRight,
+  faChartPie,
+  faCompactDisc,
+  faLaptopCode,
+  faDesktop,
+} from "@fortawesome/free-solid-svg-icons";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDeskpro } from "@fortawesome/free-brands-svg-icons";
 
 export default function Curriculum() {
   return (
@@ -21,51 +22,56 @@ export default function Curriculum() {
       </Head>
 
       <Layout>
-        <div className="mx-auto max-w-2xl text-justify text-white">
-          <div className="text-5xl mb-4 mt-10 font-bold text-white">
-            Profile
+        <div className="mx-auto max-w-2xl text-gray-200">
+          <div className="">
+            <div className="text-5xl mb-3 font-bold text-green-300">
+              Profile
+            </div>
+            <div className="text-xl text-gray-200">
+              <p className="mb-4">
+                Multi-disciplinary professional, deeply intrigued by the process
+                of learning and passionate about telling stories through data.
+              </p>
+              <p className="mb-4">
+                Strong statistical background and experience applying data
+                science techniques to solve complex problems.
+              </p>
+              <p className="mb-4">
+                Developed a very wide skill set, ranging from computer science
+                and design to multimedia creation and processing.
+              </p>
+            </div>
           </div>
-          Multi-disciplinary professional, deeply intrigued by the process of
-          learning and passionate about telling stories through data. Strong
-          statistical background and experience applying data science techniques
-          to solve complex problems. Developed a very wide skill set, ranging
-          from computer science and design to multimedia creation and
-          processing.
-          <ul className="list-inside list-disc">
-            <li className="list-item">Adobe Illustrator</li>
-            <li className="list-item">Photoshop</li>
-            <li className="list-item">Davinci Resolve</li>
-            <li className="list-item">Logic Pro</li>
-            <li className="list-item">LaTeX</li>
-          </ul>
-          <div className="text-4xl mb-4 mt-10 font-bold text-white">
-            Experience
-            <ul className="list-inside list-disc">
+
+          <div className="mt-20">
+            <div className="text-4xl mb-3 font-bold text-purple-300">
+              Experience
+            </div>
+            <ul className="list-inside">
               <li className="list-item mt-10 text-2xl">
+                <FontAwesomeIcon icon={faChevronRight} className="mr-3" />
                 Freelance mathematical animator
               </li>
 
-              <div className="flex flex-row space-x-3">
-                <div className="flex flex-row ml-4 items-center">
+              <div className="flex flex-row space-x-5">
+                <div className="flex flex-row ml-5 items-center">
                   <FontAwesomeIcon
                     icon={faCalendar}
-                    className="transform scale-50 text-accent-1-lighter"
+                    className="mr-2 text-purple-200"
                   />
-                  <span className="text-sm font-normal text-accent-1-lighter">
+                  <span className="font-normal text-purple-200">
                     July 2021 - Present
                   </span>
                 </div>
-                <div className="flex flex-row ml-4 items-center">
+                <div className="flex flex-row ml-5 items-center">
                   <FontAwesomeIcon
                     icon={faLocationArrow}
-                    className="transform scale-50 text-accent-1-lighter"
+                    className="mr-1 text-purple-200"
                   />
-                  <span className="text-sm font-normal text-accent-1-lighter">
-                    Remote
-                  </span>
+                  <span className="font-normal text-purple-200">Remote</span>
                 </div>
               </div>
-              <ul className="list-inside list-disc mt-4 ml-4 text-sm font-normal">
+              <ul className="list-inside list-disc mt-4 ml-4 font-normal">
                 <li>
                   Working on shedding a light on how the concept of biodiversity
                   is understood in social media.
@@ -81,29 +87,30 @@ export default function Curriculum() {
                 </li>
               </ul>
               <li className="list-item mt-10 text-2xl">
+                <FontAwesomeIcon icon={faChevronRight} className="mr-3" />
                 Data analyst and engineer
               </li>
-              <div className="flex flex-row space-x-3">
-                <div className="flex flex-row ml-4 items-center">
+              <div className="flex flex-row space-x-5">
+                <div className="flex flex-row ml-5 items-center">
                   <FontAwesomeIcon
                     icon={faCalendar}
-                    className="transform scale-50 text-accent-1-lighter"
+                    className="mr-2 text-purple-200"
                   />
-                  <span className="text-sm font-normal text-accent-1-lighter">
+                  <span className="font-normal text-purple-200">
                     January 2021 - May 2021
                   </span>
                 </div>
-                <div className="flex flex-row ml-4 items-center">
+                <div className="flex flex-row ml-5 items-center">
                   <FontAwesomeIcon
                     icon={faLocationArrow}
-                    className="transform scale-50 text-accent-1-lighter"
+                    className="mr-1 text-purple-200"
                   />
-                  <span className="text-sm font-normal text-accent-1-lighter">
+                  <span className="font-normal text-purple-200">
                     Universidad de Jaén
                   </span>
                 </div>
               </div>
-              <ul className="list-inside list-disc mt-4 ml-4 text-sm font-normal">
+              <ul className="list-inside list-disc mt-4 ml-4 font-normal">
                 <li>
                   Working on shedding a light on how the concept of biodiversity
                   is understood in social media.
@@ -119,28 +126,31 @@ export default function Curriculum() {
                 </li>
               </ul>
 
-              <li className="list-item mt-10 text-2xl">Intern data engineer</li>
-              <div className="flex flex-row space-x-3">
-                <div className="flex flex-row ml-4 items-center">
+              <li className="list-item mt-10 text-2xl">
+                <FontAwesomeIcon icon={faChevronRight} className="mr-3" />
+                Intern data engineer
+              </li>
+              <div className="flex flex-row space-x-5">
+                <div className="flex flex-row ml-5 items-center">
                   <FontAwesomeIcon
                     icon={faCalendar}
-                    className="transform scale-50 text-accent-1-lighter"
+                    className="mr-2 text-purple-200"
                   />
-                  <span className="text-sm font-normal text-accent-1-lighter">
+                  <span className="font-normal text-purple-200">
                     September 2019 - May 2020
                   </span>
                 </div>
-                <div className="flex flex-row ml-4 items-center">
+                <div className="flex flex-row ml-5 items-center">
                   <FontAwesomeIcon
                     icon={faLocationArrow}
-                    className="transform scale-50 text-accent-1-lighter"
+                    className="mr-1 text-purple-200"
                   />
-                  <span className="text-sm font-normal text-accent-1-lighter">
+                  <span className="font-normal text-purple-200">
                     Universidad de Jaén
                   </span>
                 </div>
               </div>
-              <ul className="list-inside list-disc mt-4 ml-4 text-sm font-normal">
+              <ul className="list-inside list-disc mt-4 ml-4 font-normal">
                 <li>
                   Assisting and learning in the process of gathering and
                   arranging data
@@ -149,15 +159,97 @@ export default function Curriculum() {
               </ul>
             </ul>
           </div>
-          <div></div>
-          <div className="text-4xl mb-4 mt-10 font-bold text-white">
-            Education
+
+          <div className="mt-20">
+            <div className="text-4xl mb-4 font-bold text-blue-400">
+              Education
+            </div>
+
+            <ul>
+              <li className="list-item mt-10 text-2xl">
+                <FontAwesomeIcon icon={faChartPie} className="mr-3" />
+                MSc in Data Science
+              </li>
+              <div className="flex flex-row space-x-5">
+                <div className="flex flex-row ml-5 items-center">
+                  <FontAwesomeIcon
+                    icon={faCalendar}
+                    className="mr-2 text-blue-400"
+                  />
+                  <span className="font-normal text-blue-400">
+                    September 2020 - Jun 2021
+                  </span>
+                </div>
+                <div className="flex flex-row ml-5 items-center">
+                  <FontAwesomeIcon
+                    icon={faLocationArrow}
+                    className="mr-1 text-blue-400"
+                  />
+                  <span className="font-normal text-blue-400">
+                    Universidad de Granada
+                  </span>
+                </div>
+              </div>
+              <ul className="list-inside list-disc mt-4 ml-4 font-normal">
+                <li>
+                  Thesis in NLP based transformer to generate medical comments
+                  and evaluate the performance of state of the art medical-text
+                  classifiers.
+                </li>
+                <li>GPA 8.5/10</li>
+                <li>
+                  Coding of advanced data mining algorithms using Python and R.
+                </li>
+                <li>Tidyverse, ggplot2, Spacy, Keras/Tensorflow.</li>
+                <li>
+                  Statistical models, regression, fuzzy logic, data mining
+                  algorithms, clustering and classification.
+                </li>
+              </ul>
+
+              <li className="list-item mt-10 text-2xl">
+                <FontAwesomeIcon icon={faDesktop} className="mr-3" />
+                BSc in Computer Science
+              </li>
+              <div className="flex flex-row space-x-5">
+                <div className="flex flex-row ml-5 items-center">
+                  <FontAwesomeIcon
+                    icon={faCalendar}
+                    className="mr-2 text-blue-400"
+                  />
+                  <span className="font-normal text-blue-400">
+                    September 2016 - Jun 2020
+                  </span>
+                </div>
+                <div className="flex flex-row ml-5 items-center">
+                  <FontAwesomeIcon
+                    icon={faLocationArrow}
+                    className="mr-1 text-blue-400"
+                  />
+                  <span className="font-normal text-blue-400">
+                    Universidad de Jaén
+                  </span>
+                </div>
+              </div>
+              <ul className="list-inside list-disc mt-4 ml-4 font-normal">
+                <li>
+                  Maximum grade in bachelor's thesis building a convolutional
+                  autoencoder to detect anomalies in images for the WWF using
+                  Tensorflow and OpenCV.
+                </li>
+                <li>
+                  GPA of 7.5/10 and passed with honors Image Processing and Data
+                  Engineering.
+                </li>
+                <li>
+                  Data structures and Algorithms using C++ and Python,
+                  Artificial Intelligence, Operating Systems.
+                </li>
+              </ul>
+            </ul>
           </div>
-          <div></div>
+
           <div className="text-4xl mb-4 mt-10 font-bold text-white">Skills</div>
-          <div></div>
-          <div></div>
-          <div></div>
         </div>
       </Layout>
     </>
