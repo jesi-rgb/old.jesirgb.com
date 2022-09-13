fc575507e9338957e546e82c2d454cfa0e7bcccb
Jesús Enrique Rascón <blograso@gmail.com>
2022-09-13 23:00:21 +0200

    minor style changes

--- components/Footer.js
+++ components/Footer.js
@@ -20,7 +20,7 @@ export default function Footer() {
     <footer className="border-t border-gray-200 text-gray-300 mt-32">
       <div className="py-8 space-y-9 md:space-y-0 md:py-10 flex flex-col md:flex-row items-center">
         <Link href="/">
-          <a className="text-xl mb-2 md:my-0 md:w-2/6 font-bold tracking-tighter leading-tight text-left hover:text-accent-1 transition-colors">
+          <a className="text-xl mb-2 md:my-0 md:w-2/6 font-bold tracking-tighter leading-tight text-left  hover:text-accent-1-lighter-lighter transition-colors">
             <h3>Jesús Rascón</h3>
           </a>
         </Link>
@@ -30,7 +30,7 @@ export default function Footer() {
             <a>
               <FontAwesomeIcon
                 icon={faInstagram}
-                className="transform scale-150 hover:text-accent-1 transition-colors"
+                className="transform scale-150  hover:text-accent-1-lighter transition-colors"
               />
             </a>
           </Link>
@@ -39,7 +39,7 @@ export default function Footer() {
             <a>
               <FontAwesomeIcon
                 icon={faTwitter}
-                className="transform scale-150 hover:text-accent-1 transition-colors"
+                className="transform scale-150  hover:text-accent-1-lighter transition-colors"
               />
             </a>
           </Link>
@@ -48,7 +48,7 @@ export default function Footer() {
             <a>
               <FontAwesomeIcon
                 icon={faGithub}
-                className="transform scale-150 hover:text-accent-1 transition-colors"
+                className="transform scale-150  hover:text-accent-1-lighter transition-colors"
               />
             </a>
           </Link>
@@ -56,7 +56,7 @@ export default function Footer() {
             <a>
               <FontAwesomeIcon
                 icon={faAt}
-                className="transform scale-150 hover:text-accent-1 transition-colors"
+                className="transform scale-150  hover:text-accent-1-lighter transition-colors"
               />
             </a>
           </Link>
@@ -65,7 +65,7 @@ export default function Footer() {
             <a>
               <FontAwesomeIcon
                 icon={faLinkedin}
-                className="transform scale-150 hover:text-accent-1 transition-colors"
+                className="transform scale-150  hover:text-accent-1-lighter transition-colors"
               />
             </a>
           </Link>
@@ -73,12 +73,12 @@ export default function Footer() {
             <a>
               <FontAwesomeIcon
                 icon={faTelegram}
-                className="transform scale-150 hover:text-accent-1 transition-colors"
+                className="transform scale-150  hover:text-accent-1-lighter transition-colors"
               />
             </a>
           </Link>
         </div>
-        <div className="text-2xl md:text-xl my-3 md:w-2/6 md:my-0 text-right">
+        <div className="text-2xl font-bold md:text-xl my-3 md:w-2/6 md:my-0 text-right">
           {new Date().getFullYear()}
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -29,100 +29,102 @@ export default function Landing() {
         <title>Jesús Rascón</title>
       </Head>
 
-      <section className="text-white mb-10 xl:mt-20">
-        <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-          <div className="text-center lg:w-2/3 w-full">
-            <h1 className="text-5xl xl:text-7xl mb-4 font-bold font-blogpost text-white">
-              Jesús Enrique Rascón
-            </h1>
-            <p className="mb-8 mt-10 leading-relaxed text-lg xl:text-2xl font-display text-opacity-60">
-              I am a freelance mathematical animator at{" "}
-              <a
-                className="font-bold text-accent-1-lighter hover:text-accent-1"
-                href="https://www.youtube.com/c/Reducible"
-              >
-                <FontAwesomeIcon icon={faCubes} className="" /> Reducible
-              </a>
-              . Prev at{" "}
-              <a
-                className="font-bold text-accent-1-lighter hover:text-accent-1"
-                href="https://www.youtube.com/c/Veritasium"
-              >
-                Veritasium
-              </a>
-              .
-            </p>
+      <html id="index">
+        <section className="text-white mb-10 xl:mt-20">
+          <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+            <div className="text-center lg:w-2/3 w-full">
+              <h1 className="text-5xl xl:text-7xl mb-4 font-bold font-blogpost text-white">
+                Jesús Enrique Rascón
+              </h1>
+              <p className="mb-8 mt-10 leading-relaxed text-lg xl:text-2xl font-display text-opacity-60">
+                I am a freelance mathematical animator at{" "}
+                <a
+                  className="font-bold text-accent-1-lighter hover:text-accent-1"
+                  href="https://www.youtube.com/c/Reducible"
+                >
+                  <FontAwesomeIcon icon={faCubes} className="" /> Reducible
+                </a>
+                . Prev at{" "}
+                <a
+                  className="font-bold text-accent-1-lighter hover:text-accent-1"
+                  href="https://www.youtube.com/c/Veritasium"
+                >
+                  Veritasium
+                </a>
+                .
+              </p>
+            </div>
+            <div className="text-center mt-10 flex flex-col space-y-5 xl:flex-row xl:space-x-9 xl:space-y-0">
+              <LandingButton link="/portfolio" icon={faBook} text="Portfolio" />
+              <LandingButton
+                link="/curriculum"
+                icon={faAward}
+                text="Curriculum"
+              />
+              <LandingButton link="/blog" icon={faICursor} text="Blog" />
+              <LandingButton link="/about" icon={faUser} text="About" />
+            </div>
+
+            <div className="text-accent-1-lighter flex flex-row justify-center space-x-8 xl:space-x-9 items-center my-2 mt-20 md:w-2/6">
+              <Link href="https://www.instagram.com/jrascon_">
+                <a>
+                  <FontAwesomeIcon
+                    icon={faInstagram}
+                    className="transform scale-150 hover:text-accent-1-lighter transition-colors"
+                  />
+                </a>
+              </Link>
+
+              <Link href="https://www.twitter.com/jesi_rgb">
+                <a>
+                  <FontAwesomeIcon
+                    icon={faTwitter}
+                    className="transform scale-150 hover:text-accent-1-lighter transition-colors"
+                  />
+                </a>
+              </Link>
+
+              <Link href="https://www.github.com/jesi-rgb">
+                <a>
+                  <FontAwesomeIcon
+                    icon={faGithub}
+                    className="transform scale-150 hover:text-accent-1-lighter transition-colors"
+                  />
+                </a>
+              </Link>
+              <Link href="mailto:blograso@gmail.com">
+                <a>
+                  <FontAwesomeIcon
+                    icon={faAt}
+                    className="transform scale-150 hover:text-accent-1-lighter transition-colors"
+                  />
+                </a>
+              </Link>
+
+              <Link href="https://www.linkedin.com/in/jesi-rgb/">
+                <a>
+                  <FontAwesomeIcon
+                    icon={faLinkedin}
+                    className="transform scale-150 hover:text-accent-1-lighter transition-colors"
+                  />
+                </a>
+              </Link>
+              <Link href="https://t.me/jrascon">
+                <a>
+                  <FontAwesomeIcon
+                    icon={faTelegram}
+                    className="transform scale-150 hover:text-accent-1-lighter transition-colors"
+                  />
+                </a>
+              </Link>
+            </div>
           </div>
-          <div className="text-center mt-10 flex flex-col space-y-5 xl:flex-row xl:space-x-9 xl:space-y-0">
-            <LandingButton link="/portfolio" icon={faBook} text="Portfolio" />
-            <LandingButton
-              link="/curriculum"
-              icon={faAward}
-              text="Curriculum"
-            />
-            <LandingButton link="/blog" icon={faICursor} text="Blog" />
-            <LandingButton link="/about" icon={faUser} text="About" />
-          </div>
+        </section>
 
-          <div className="text-gray-400 flex flex-row justify-center space-x-8 xl:space-x-9 items-center my-2 mt-20 md:w-2/6">
-            <Link href="https://www.instagram.com/jrascon_">
-              <a>
-                <FontAwesomeIcon
-                  icon={faInstagram}
-                  className="transform scale-150 hover:text-accent-1-lighter transition-colors"
-                />
-              </a>
-            </Link>
-
-            <Link href="https://www.twitter.com/jesi_rgb">
-              <a>
-                <FontAwesomeIcon
-                  icon={faTwitter}
-                  className="transform scale-150 hover:text-accent-1-lighter transition-colors"
-                />
-              </a>
-            </Link>
-
-            <Link href="https://www.github.com/jesi-rgb">
-              <a>
-                <FontAwesomeIcon
-                  icon={faGithub}
-                  className="transform scale-150 hover:text-accent-1-lighter transition-colors"
-                />
-              </a>
-            </Link>
-            <Link href="mailto:blograso@gmail.com">
-              <a>
-                <FontAwesomeIcon
-                  icon={faAt}
-                  className="transform scale-150 hover:text-accent-1-lighter transition-colors"
-                />
-              </a>
-            </Link>
-
-            <Link href="https://www.linkedin.com/in/jesi-rgb/">
-              <a>
-                <FontAwesomeIcon
-                  icon={faLinkedin}
-                  className="transform scale-150 hover:text-accent-1-lighter transition-colors"
-                />
-              </a>
-            </Link>
-            <Link href="https://t.me/jrascon">
-              <a>
-                <FontAwesomeIcon
-                  icon={faTelegram}
-                  className="transform scale-150 hover:text-accent-1-lighter transition-colors"
-                />
-              </a>
-            </Link>
-          </div>
+        <div className="hidden lg:block md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl mx-auto">
+          <TopTracks />
         </div>
-      </section>
-
-      <div className="hidden lg:block md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl mx-auto">
-        <TopTracks />
-      </div>
+      </html>
     </>
   );
 }
