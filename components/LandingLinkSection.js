dc17d117bbc6bfb815b0046713addb2ba3f28750
Jesús Rascón <blograso@gmail.com>
2023-02-18 14:15:24 +0100

    looking good

---
+++ components/LandingLinkSection.js
@@ -0,0 +1,10 @@
+export default function LandingLinkSection() {
+  return (
+    <div className="text-white my-10 text-left text-9xl font-display w-1/2 mx-auto">
+      <div>Blog</div>
+      <div>Portfolio</div>
+      <div>Curriculum</div>
+      <div>About</div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,7 @@ import {
   faTelegram,
 } from "@fortawesome/free-brands-svg-icons";
 import LandingButton from "@/components/LandingButton";
+import LandingLinkSection from "@/components/LandingLinkSection";
 
 export default function Landing() {
   return (
@@ -29,101 +30,39 @@ export default function Landing() {
       </Head>
 
       <html id="index">
-        <section className="text-white mb-10 md:mt-20">
-          <div className="container mb mx-auto flex px-5 py-24 items-center justify-center flex-col">
-            <div className="text-center">
-              <h1 className="text-7xl md:text-9xl leading-none tracking-wide mb-4 font-bold font-display text-white">
-                Jesús Enrique Rascón
-              </h1>
-              <div className="mb-8 mt-10 leading-relaxed text-xl xl:text-2xl font-sans w-full md:w-3/4 mx-auto">
-                <p>
-                  I am a freelance mathematical animator at{" "}
-                  <a
-                    className="font-bold text-accent-1-lighter hover:text-accent-1 transition-all"
-                    href="https://www.youtube.com/c/Reducible"
-                  >
-                    <FontAwesomeIcon icon={faCubes} className="" /> Reducible
-                  </a>
-                  . Previously at{" "}
-                  <a
-                    className="font-bold text-accent-1-lighter hover:text-accent-1 transition-all"
-                    href="https://www.youtube.com/c/Veritasium"
-                  >
-                    Veritasium
-                  </a>
-                  .
-                </p>
-                <p>
-                  I am also a web developer and designer.
-                </p>
-              </div>
-            </div>
-            <div className="text-center mt-10 flex flex-col space-y-5 md:flex-row md:space-x-9 md:space-y-0">
-              <LandingButton link="/portfolio" icon={faBook} text="Portfolio" />
-              <LandingButton
-                link="/curriculum"
-                icon={faAward}
-                text="Curriculum"
-              />
-              <LandingButton link="/blog" icon={faICursor} text="Blog" />
-              <LandingButton link="/about" icon={faUser} text="About" />
-            </div>
+        <section className="text-white mb-10 md:mt-20 h-screen">
+          <div className="w-1/2 mx-auto py-24">
+            <h1 className="text-7xl md:text-9xl text-left leading-none tracking-wide mb-4 font-bold font-display text-white">
+              Jesús Enrique Rascón
+            </h1>
+            <div className="mb-8 mt-10 text-left leading-relaxed text-xl xl:text-2xl font-sans w-full">
+              <p>
+                I am a freelance mathematical animator at{" "}
+                <a
+                  className="font-bold text-accent-1-lighter hover:text-accent-1 transition-all"
+                  href="https://www.youtube.com/c/Reducible"
+                >
+                  <FontAwesomeIcon icon={faCubes} className="" /> Reducible
+                </a>
+                .
+              </p>
 
-            <div className="text-accent-1-lighter flex flex-row justify-center space-x-8 xl:space-x-9 items-center my-2 mt-20 md:w-2/6">
-              <Link href="https://www.instagram.com/jrascon_">
-                <a>
-                  <FontAwesomeIcon
-                    icon={faInstagram}
-                    className="transform scale-150 hover:text-accent-1 transition-colors"
-                  />
+              <p>
+                Previously at{" "}
+                <a
+                  className="font-bold text-accent-1-lighter hover:text-accent-1 transition-all"
+                  href="https://www.youtube.com/c/Veritasium"
+                >
+                  Veritasium
                 </a>
-              </Link>
-
-              <Link href="https://www.twitter.com/jesi_rgb">
-                <a>
-                  <FontAwesomeIcon
-                    icon={faTwitter}
-                    className="transform scale-150 hover:text-accent-1 transition-colors"
-                  />
-                </a>
-              </Link>
-
-              <Link href="https://www.github.com/jesi-rgb">
-                <a>
-                  <FontAwesomeIcon
-                    icon={faGithub}
-                    className="transform scale-150 hover:text-accent-1 transition-colors"
-                  />
-                </a>
-              </Link>
-              <Link href="mailto:blograso@gmail.com">
-                <a>
-                  <FontAwesomeIcon
-                    icon={faAt}
-                    className="transform scale-150 hover:text-accent-1 transition-colors"
-                  />
-                </a>
-              </Link>
-
-              <Link href="https://www.linkedin.com/in/jesi-rgb/">
-                <a>
-                  <FontAwesomeIcon
-                    icon={faLinkedin}
-                    className="transform scale-150 hover:text-accent-1 transition-colors"
-                  />
-                </a>
-              </Link>
-              <Link href="https://t.me/jrascon">
-                <a>
-                  <FontAwesomeIcon
-                    icon={faTelegram}
-                    className="transform scale-150 hover:text-accent-1 transition-colors"
-                  />
-                </a>
-              </Link>
+                .
+              </p>
+              <p>I am also a web developer and designer.</p>
             </div>
           </div>
         </section>
+
+        <LandingLinkSection />
       </html>
     </>
   );
