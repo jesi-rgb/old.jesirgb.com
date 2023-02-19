6bcb80689c97667b17fac6cceb431c48b170f58c
Jesús Rascón <blograso@gmail.com>
2023-02-19 21:30:39 +0100

    nice

--- components/portfolio/IndexPortfolioItem.js
+++ components/portfolio/IndexPortfolioItem.js
@@ -4,12 +4,16 @@ export default function IndexPortfolioItem({ category, icon }) {
   return (
     <a
       href={"#" + category.toLowerCase().replace(" ", "-")}
-      className="group hover:bg-accent-1-hover transition-colors hover:bg-opacity-50"
+      style={{ filter: "" }}
+      className="group hover:bg-accent-1-hover rounded-xl hover:bg-opacity-50 hover:-translate-y-1 hover:animate-pulse transform transition-all duration-500"
     >
-      <div className="px-5 py-4 h-full w-fit rounded-lg border-2 flex flex-col items-center justify-between">
+      <div className="px-5 py-4 h-full w-fit rounded-xl border-2 flex flex-col items-center justify-between">
         <span className="text-white group-hover:text-accent-1-lighter hover:rotate-12 transform transition-all text-5xl">
           <FontAwesomeIcon
-            style={{ filter: "drop-shadow(0px 0px 4px #AEF7D088)" }}
+            style={{
+              filter:
+                "drop-shadow(0px 0px 4px #AEF7D088) drop-shadow(2px 2px 0px #AEF7D066)",
+            }}
             icon={icon}
           />
         </span>
