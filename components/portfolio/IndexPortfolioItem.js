e479803931673a72f3f753a929fd7e52b54ba3f7
Jesús Rascón <blograso@gmail.com>
2023-02-19 19:02:31 +0100

    damn shiuiny

--- components/portfolio/IndexPortfolioItem.js
+++ components/portfolio/IndexPortfolioItem.js
@@ -8,7 +8,10 @@ export default function IndexPortfolioItem({ category, icon }) {
     >
       <div className="px-5 py-4 h-full w-fit rounded-lg border-2 flex flex-col items-center justify-between">
         <span className="text-white group-hover:text-accent-1-lighter hover:rotate-12 transform transition-all text-5xl">
-          <FontAwesomeIcon icon={icon} />
+          <FontAwesomeIcon
+            style={{ filter: "drop-shadow(0px 0px 4px #AEF7D088)" }}
+            icon={icon}
+          />
         </span>
         <span className="text-gray-100 leading-none font-display font-bold text-4xl md:text-6xl text-center group-hover:text-accent-1-lighter transition-all mt-5">
           {category}
