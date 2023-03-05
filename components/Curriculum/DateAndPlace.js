4ed446ba0e1c8421064a93878ca4567e98c16624
Jesús Rascón <blograso@gmail.com>
2023-03-05 20:48:27 +0100

    fix weird w-max thingy

--- components/Curriculum/DateAndPlace.js
+++ components/Curriculum/DateAndPlace.js
@@ -6,7 +6,7 @@ export default function DateAndPlace({ period, place = "Remote" }) {
     <>
       <div
         className={
-          "mt-3 font-mono text-sm bg-accent-1-hover bg-opacity-30 ring-2 ring-accent-1-lighter ring-opacity-70 rounded-xl px-4 py-1 w-max flex flex-row items-center space-x-5"
+          "ml-4 flex flex-col md:flex-row mt-3 font-mono text-sm bg-accent-1-hover bg-opacity-30 ring-2 ring-accent-1-lighter ring-opacity-70 rounded-xl px-3 py-1 w-max space-y-2 md:space-y-0 md:items-center md:space-x-5"
         }
       >
         <div>
